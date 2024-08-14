599071b95da3e5643bf2a28723e49c6933e7c67c
omer.kosar.33 <omer.kosar.33@gmail.com>
2024-08-15 00:43:39 +0300

    initialize auction service api project

---
+++ AuctionService/Program.cs
@@ -0,0 +1,23 @@
+
+namespace AuctionService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
