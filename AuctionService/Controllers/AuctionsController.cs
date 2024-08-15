50a558b807f3e9395ededfe28568e377edbfcb5a
omer.kosar.33 <omer.kosar.33@gmail.com>
2024-08-15 22:14:06 +0300

    Add UpdateAuction method and make Year/Mileage optional
    Added a new UpdateAuction method to the AuctionsController class,
    enabling updates to existing auctions by ID. This method uses the
    UpdateAuctionDto to update item details and save changes to the
    database. Modified the UpdateAuctionDto class to make the Year and
    Mileage properties optional, allowing partial updates where these
    fields can be omitted if no changes are needed.

--- AuctionService/Controllers/AuctionsController.cs
+++ AuctionService/Controllers/AuctionsController.cs
@@ -50,5 +50,24 @@ namespace AuctionService.Controllers
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetAuction), new { id = auction.Id }, _mapper.Map<AuctionDto>(auction));
         }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+        {
+            var auction = await _context.Auctions
+                .Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (auction == null)
+            {
+                return NotFound();
+            }
+            auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+            auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+            auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+            auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
