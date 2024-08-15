c37d94ddbee0fb52a91c974cbff0010202d50d37
omer.kosar.33 <omer.kosar.33@gmail.com>
2024-08-15 23:14:29 +0300

    Add SearchService project and MongoDB service
    Updated Carsties.sln to include the new SearchService project. Modified docker-compose.yml to add a MongoDB service with necessary configurations. Added appsettings.Development.json and appsettings.json for logging and host configurations. Created Program.cs to set up the ASP.NET Core application for SearchService. Added SearchService.csproj to define project structure and dependencies. Included SearchService.http for testing HTTP requests. Configured launchSettings.json for development environment.

---
+++ SearchService/Program.cs
@@ -0,0 +1,23 @@
+
+namespace SearchService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseAuthorization();
+  
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
