39dbd8c43fa8e9eee057066040505a5e1fedfe20
omer.kosar.33 <omer.kosar.33@gmail.com>
2024-08-16 23:25:10 +0300

    Add SearchController  Added a new SearchController to handle HTTP GET requests at the route api/search. The controller uses MongoDB's PagedSearch to search for items, supports pagination, and sorts results by the Make property. Included necessary using directives for ASP.NET Core MVC, MongoDB Entities, and SearchService.Models.

---
+++ SearchService/Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<Item>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
+        {
+            var query = DB.PagedSearch<Item>();
+            query.Sort(x => x.Ascending(x => x.Make));
+
+            if (!string.IsNullOrEmpty(searchTerm))
+                query.Match(Search.Full, searchTerm);
+
+            query.PageSize(pageSize);
+            query.PageNumber(pageNumber);
+
+            var result = await query.ExecuteAsync();
+
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            });
+        }
+    }
+}
